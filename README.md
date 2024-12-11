610541aededd8a42189c09a3b8203b79c9dab868
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-12-11 15:10:16 +0200

    Add README file

---
+++ README.md
@@ -0,0 +1,11 @@
+# WebScraper for Lenovo devices
+
+### Run
+- Place the input ```.txt``` file inside ```data/input/```
+- Run ```python3 scrape.py``` (inside the ```src``` folder)
+- See the output inside ````data/output/````. There are 2 files that contain the same data, one in ```.csv``` and one in ```.xlsx``` formats.
+
+### Tech Stack
+- python3
+- selenium
+- openpyxl
--- src/scrape.py
+++ src/scrape.py
@@ -8,9 +8,14 @@ from selenium.webdriver.common.by import By
 import csv
 from selenium.webdriver.chrome.options import Options
 
+from openpyxl import Workbook
+from openpyxl.styles import Font
+
 # Settings
 PRINT_TO_CONSOLE = True
-HIDE_BROWSER = False
+HIDE_BROWSER = True
+DELIMITER = ';'
+SEARCH_TIMEOUT = 1
 
 # input and output files
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -20,9 +25,11 @@ OUTPUT_DIR = os.path.join(PROJECT_ROOT, 'data', 'output')
 
 input_file_name = 'DeviceModel.txt'
 output_file_name = 'DeviceDetails.csv'
+final_output_file_name = 'DeviceDetails.xlsx'
 
 input_file_path = os.path.join(INPUT_DIR, input_file_name)
 output_file_path = os.path.join(OUTPUT_DIR, output_file_name)
+final_output_file_path = os.path.join(OUTPUT_DIR, final_output_file_name)
 
 # the search page URL
 BASE_URL = "https://psref.lenovo.com/search/"
@@ -35,9 +42,14 @@ if HIDE_BROWSER:
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(BASE_URL)
 
+counter = 1
+
 def search_ID(id):
+    global counter
+
     if PRINT_TO_CONSOLE:
-        print(f"Searching for: {id}")
+        print(f"{counter}. Searching for: {id}")
+        counter += 1
 
     search_box = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CLASS_NAME, "as_quick_search_text"))
@@ -57,14 +69,15 @@ def print_device_details(device_ID, device_name, device_link, device_processor, 
         print(f"Product Storage: {device_storage}")
         print("=====================================================================================\n")
 
-with open(output_file_path, 'w', newline='', encoding='utf-8') as csvfile:
-    csv_writer = csv.writer(csvfile)
+with open(output_file_path, 'w', newline='', encoding='utf-8-sig') as csvfile:
+    # csv_writer = csv.writer(csvfile)
+    csv_writer = csv.writer(csvfile, delimiter=DELIMITER)
     csv_writer.writerow(["ID", "Name", "Link", "Processor", "Memory", "Storage"])
 
 try:
-    with open(input_file_path, 'r') as file, open(output_file_path, 'a', newline='', encoding='utf-8') as csvfile:
+    with open(input_file_path, 'r') as file, open(output_file_path, 'a', newline='', encoding='utf-8-sig') as csvfile:
         # the writer
-        csv_writer = csv.writer(csvfile)
+        csv_writer = csv.writer(csvfile, delimiter=DELIMITER)
 
         # for each line, remove leading/trailing whitespace and newline characters and skip empty lines
         for line in file:
@@ -83,7 +96,7 @@ try:
             search_ID(device_ID)
 
             # wait for the results
-            time.sleep(1)
+            time.sleep(SEARCH_TIMEOUT)
 
             # Wait for the product_array div
             product_array = WebDriverWait(driver, 10).until(
@@ -141,3 +154,21 @@ except FileNotFoundError:
     print(f"File {input_file_name} not found in {INPUT_DIR}.")
 finally:
     driver.quit()
+
+# get xlsx from csv
+wb = Workbook()
+ws = wb.active
+ws.title = "Device Details"
+
+# read the CSV file and write to the Excel sheet
+with open(output_file_path, 'r', encoding='utf-8-sig') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=DELIMITER)
+    for row_index, row in enumerate(csv_reader):
+        ws.append(row)
+
+        if row_index == 0:
+            for cell in ws[row_index + 1]:
+                cell.font = Font(bold=True)
+
+# save the Excel file
+wb.save(final_output_file_path)
